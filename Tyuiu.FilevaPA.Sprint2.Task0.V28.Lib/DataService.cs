83d605e0140229ff15edb31bb2c89e9595f02276
Полина Филeва <polina_fileva@list.ru>
2025-11-15 23:57:31 +0500

    Task 0

--- Tyuiu.FilevaPA.Sprint2.Task0.V28.Lib/DataService.cs
+++ Tyuiu.FilevaPA.Sprint2.Task0.V28.Lib/DataService.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tyuiu.FilevaPA.Sprint2.Task0.V28.Lib;
 using tyuiu.cources.programming.interfaces.Sprint2;
-using Tyuiu.FilevaPA.Sprint2.Task0.V28.Lib;
+
 
 public class DataService : ISprint2Task0V28
 {
